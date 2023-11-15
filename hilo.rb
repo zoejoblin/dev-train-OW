df4b3224e49591e0ea811339766eb5ab0ba489bc
Zoe Joblin <zoejoblin@Zoes-MacBook-Pro.local>
2023-11-16 11:40:56 +1300

    allow user to guess only 6 times

--- hilo.rb
+++ hilo.rb
@@ -4,19 +4,23 @@ puts random_number
 puts "Enter a number between 1 and 100. press enter to submit your guess"
 guess = $stdin.readline()
 guess_as_int = Integer(guess)
+number_of_guesses = 0
 
-while guess_as_int != random_number
-    if guess_as_int > 100
-        puts "Your guess is too high. Try again"
-    elsif guess_as_int < 1
-        puts "Your guess is too low. Try again"
+for i in 1..5 do
+    if guess_as_int != random_number
+        if guess_as_int > 100
+            puts "Your guess is too high. Try again"
+        elsif guess_as_int < 1
+            puts "Your guess is too low. Try again"
+        else
+            puts "Incorrect! Try again"
+        end
+        guess = $stdin.readline()
+        guess_as_int = Integer(guess)
     else
-        puts "Incorrect! Try again"
+        puts "Correct! You win"
+        break
     end
-    guess = $stdin.readline()
-    guess_as_int = Integer(guess)
 end
-
-if guess_as_int == random_number
-    puts "Correct! You win"
-end
+# TODO: prevent this from running if correct number is guessed
+puts "Sorry, you ran out of guesses. The number was " << String(random_number)
