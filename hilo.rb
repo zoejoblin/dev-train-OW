4c61cfd8be74ba8f79b7039136d0fee8bc257165
Zoe Joblin <zoejoblin@Zoes-MacBook-Pro.local>
2023-11-16 10:32:28 +1300

    compare user input to random number and print success message if correct.

--- hilo.rb
+++ hilo.rb
@@ -1,4 +1,10 @@
 number = rand(1..100)
+# print random number for testing purposes
+puts number
 puts "Enter a number between 1 and 100. press enter to submit your guess"
 guess = $stdin.readline()
-puts "You typed: " << guess 
+guess_as_int = Integer(guess)
+p guess_as_int
+if guess_as_int == number
+    puts "Correct! You win"
+end
