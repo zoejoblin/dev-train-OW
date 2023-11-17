d691a244a71d58fa30c8755af184cd3131c3d23a
Zoe Joblin <zoejoblin@Zoes-MacBook-Pro.local>
2023-11-17 14:32:40 +1300

    refactor and ensure user gets only success message when they guess correctly

--- hilo.rb
+++ hilo.rb
@@ -1,26 +1,22 @@
 random_number = rand(1..100)
-# print random number for testing purposes
-puts random_number
 puts "Enter a number between 1 and 100. press enter to submit your guess"
-guess = $stdin.readline()
-guess_as_int = Integer(guess)
-number_of_guesses = 0
-
-for i in 1..5 do
-    if guess_as_int != random_number
-        if guess_as_int > 100
-            puts "Your guess is too high. Try again"
-        elsif guess_as_int < 1
-            puts "Your guess is too low. Try again"
-        else
-            puts "Incorrect! Try again"
-        end
-        guess = $stdin.readline()
-        guess_as_int = Integer(guess)
+input = gets.chomp
+guess = input.to_i
+response = ''
+count = 0
+while count < 5
+    count = count + 1
+    response = ''
+    if guess > 100
+        response = "Your guess is too high. Try again"
+    elsif guess < 1
+        response = "Your guess is either too low or is not a number. Try again"
     else
-        puts "Correct! You win"
-        break
+        response = (guess == random_number) ? break : "Incorrect! Try again"
     end
+    puts response
+    input = gets.chomp
+    guess = input.to_i
 end
-# TODO: prevent this from running if correct number is guessed
-puts "Sorry, you ran out of guesses. The number was " << String(random_number)
+response = (guess == random_number) ? "Correct! You win" : "Sorry, you ran out of guesses. The number was " << String(random_number)
+puts response
